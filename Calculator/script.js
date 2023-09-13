995b120a82e27a79efd2b7febdeabd937a1a9f72
Mayank Deep <142878404+mayank28deep@users.noreply.github.com>
2023-09-13 20:43:57 +0530

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,35 @@
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((item) => {
+    item.onclick = () => {
+        if (item.id == 'allClear') {
+            display.innerText = '';
+        } else if (item.id == 'delLastChar') {
+            let string = display.innerText.toString();
+            display.innerText = string.substr(0, string.length - 1);
+        } else if (display.innerText != '' && item.id == 'calculate') {
+            display.innerText = eval(display.innerText);
+        } else if (display.innerText == '' && item.id == 'calculate') {
+            display.innerText = 'Invalid!!';
+            setTimeout(() => (display.innerText = ''), 2000);
+        } else {
+            display.innerText += item.id;
+        }
+    }
+})
+
+const themeToggleBtn = document.querySelector('.theme-toggler');
+const calculator = document.querySelector('.calculator');
+let isDark = true;
+themeToggleBtn.onclick = () => {
+    calculator.classList.toggle('dark');
+    themeToggleBtn.classList.toggle('active');
+    isDark = !isDark;
+}
+
+var darkMode = document.getElementById('dark-change');
+darkMode.addEventListener('click',function() {
+    darkMode.classList.toggle('active');
+    content.classList.toggle('dark');
+})
